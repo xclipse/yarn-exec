9b1dafeead908c030420f470c32f75008637ed38
eclipse <eclipse@RYVIUS-PC>
2015-01-10 11:49:15 +0800

    push SingleMapper

---
+++ src/main/java/com/eclair/SingleMapper.java
@@ -0,0 +1,57 @@
+package com.eclair;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class SingleMapper extends Configured implements Tool {
+
+	@Override
+	public int run(String[] args) throws Exception {
+		if(args.length != 2){
+			System.err.printf("Usage: %s [generic options] <input> <output>\n", getClass().getSimpleName());
+			return -1;
+		}
+		Job job = Job.getInstance(getConf());
+		job.setJobName("Single Mapper");
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setInputFormatClass(KeyValueTextInputFormat.class);
+		job.setJarByClass(getClass());
+		job.setMapperClass(OneMapper.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setNumReduceTasks(0);
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int exit = ToolRunner.run(new SingleMapper(), args);
+		System.exit(exit);
+	}
+	
+	public static class OneMapper extends Mapper<Text, Text, Text, Text>{
+		private int id;
+		private static int sid = 0;
+		public OneMapper() {
+			id = sid++;
+		}
+		@Override
+		protected void map(Text key, Text value, Mapper<Text, Text, Text, Text>.Context context)
+				throws IOException, InterruptedException {
+			Text k = new Text("id=" + id + " [" +key.toString() + "]");
+			Text v = new Text();
+			context.write(k,value);
+		}
+	}
+}
